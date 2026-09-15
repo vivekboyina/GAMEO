50c2fa6c4afb13085a1db323c263cf6dec45ad0a
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-09-15 21:21:36 +0530

    Change last grid cell color to green

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ for(let i = 0; i < n; i++)
         grid.appendChild(child)
         childs[i][j] = child
         if(Math.random() < 0.2) childs[i][j].textContent = rock
+        if(i == n - 1 && j == n - 1) childs[i][j].style.backgroundColor = 'green'
     }
 }
 let bot = '👽'
